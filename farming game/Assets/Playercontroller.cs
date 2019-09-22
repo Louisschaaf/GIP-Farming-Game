7f4716891d010f8d87ec2bfb35c96e7d44eec3c0
Louisschaaf <Louisschaaf03@gmail.com>
2019-09-22 20:51:20 +0200

    game
    de game tot dusver

---
+++ farming game/Assets/Playercontroller.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour
+{
+    [Header("character attributes:")]
+    public float Speed = 1.0f;
+
+    [Space]
+    [Header("character statistics:")]
+    public Vector2 movementDirection;
+    public float movementspeed;
+
+    [Space]
+    [Header("references:")]
+    public Rigidbody2D rb;
+    public Animator animator;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+        Move();
+        Animate();
+    }
+
+    void ProcessInputs()
+    {
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movementspeed = Mathf.Clamp(movementDirection.magnitude, 0.0f, 1.0f);
+        movementDirection.Normalize();
+    }
+    void Move()
+    {
+        rb.velocity = movementDirection * movementspeed * Speed;
+    } 
+
+    void Animate()
+    {
+        animator.SetFloat("Horizontal", movementDirection.x);
+        animator.SetFloat("Vertical", movementDirection.y);
+    }
+}
