f38abb4fed62bf2b378ffc0d7443687c556cf061
Devy Leskens <50657081+DevyLeskens@users.noreply.github.com>
2020-03-25 15:46:18 +0100

    NPC, Menu, Dialogue
    Made a little improvement of the menu-system. Made new script which work with the NPC. Animated all the NPC's movement and finally added them into our main scene. Now working on the dialogues and interaction with the player.

---
+++ farming game/Assets/Scripts/Interactables/Signal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Signal", menuName = "Own Signal")]
+public class Signal : ScriptableObject
+{
+    public List<SignalListener> listeners = new List<SignalListener>();
+
+    public void Raise()
+    {
+        for(int i = listeners.Count - 1; i >= 0; i--)
+        {
+            listeners[i].OnSignalRaised();
+        }
+    }
+
+    public void RegisterListener(SignalListener listener)
+    {
+        listeners.Add(listener);
+    }
+
+    public void DeRegisterListener(SignalListener listener)
+    {
+        listeners.Remove(listener);
+    }
+}
